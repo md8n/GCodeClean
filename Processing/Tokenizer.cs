62c23c7a5ed5e961e0b602aafdad50557e6f2149
Lee Humphries <lee@md8n.com>
2020-05-05 22:35:46 +0900

    Even more improved regex

--- Processing/Tokenizer.cs
+++ Processing/Tokenizer.cs
@@ -8,19 +8,37 @@ using System.Text.RegularExpressions;
 namespace GCodeClean.Processing
 {
     public static class Tokenizer {
-        // A 'basic' gcode parser pattern, this does not support expressions that equate to numbers
-        public const string pattern = @"((\%)|((?<linenumber>N\s*\d{1,5})?\s*(?<word>[ABCDFGHIJKLMNPRSTXYZ]\s*[+-]?\d*\.?\d*\s*)|(?<comment>\(.*\)\s*)))";
+        /// <summary>
+        /// A 'basic' gcode parser pattern, this does not support expressions that equate to numbers
+        /// </summary>
+        public const string pattern = @"((\%)|((?<linenumber>N\s*\d{1,5})?\s*(?<word>[ABCDFGHIJKLMNPRSTXYZ]\s*[+-]?(\d|\s)*\.?(\d|\s)*\s*)|(?<comment>\(.*\)\s*)))";
+
 
         public static async IAsyncEnumerable<List<string>> Tokenize(this IAsyncEnumerable<string> lines) {
             await foreach (var line in lines) {
-                var tokens = new List<string>();
-                foreach (Match match in Regex.Matches(line, pattern, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture))
-                {
-                    tokens.Add(match.Value.Trim());
-                }
-
-                yield return tokens;
+                yield return line.Tokenize();
             }
+        }
+
+        public static List<string> Tokenize(this string line) {
+            var tokens = new List<string>();
+
+            foreach (Match match in Regex.Matches(line, pattern, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture))
+            {
+                var token = match.Value.Trim();
+                // if this isn't a comment then strip out all spaces
+                if (!token.StartsWith('(')) {
+                    token.Replace(" ", "");
+                    decimal number;
+                    if (token.Length == 1 || !decimal.TryParse(token.Substring(1), out number)) {
+                        // Invalid command or argument - doesn't have a valid number
+                        continue;
+                    }
+                }
+                tokens.Add(token);
+            }
+
+            return tokens;
         }
 
         public static async IAsyncEnumerable<string> JoinTokens(this IAsyncEnumerable<List<string>> tokenizedLines) {
