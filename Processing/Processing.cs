c42eadf44b83a7b72490cc3efdfbf2e0238e51f3
Lee Humphries <lee@md8n.com>
2020-05-10 18:10:49 +0900

    Better augmenting

--- Processing/Processing.cs
+++ Processing/Processing.cs
@@ -10,16 +10,20 @@ using Newtonsoft.Json.Linq;
 
 namespace GCodeClean.Processing
 {
-    public static class Processing {
-        public static async IAsyncEnumerable<List<string>> Clip(this IAsyncEnumerable<List<string>> tokenizedLines) {
+    public static class Processing
+    {
+        public static async IAsyncEnumerable<List<string>> Clip(this IAsyncEnumerable<List<string>> tokenizedLines)
+        {
             JObject tokenDefinitions = JObject.Parse(File.ReadAllText("tokenDefinitions.json"));
 
             var replacements = tokenDefinitions["replacements"];
             var tokenDefs = tokenDefinitions["tokenDefs"];
             var context = new Dictionary<string, string>();
 
-            await foreach (var tokens in tokenizedLines) {
-                if (tokens.IsNotCommandOrArguments()) {
+            await foreach (var tokens in tokenizedLines)
+            {
+                if (tokens.IsNotCommandOrArguments())
+                {
                     yield return tokens;
                     continue;
                 }
@@ -51,7 +55,7 @@ namespace GCodeClean.Processing
                         {
                             // Round to 3dp for mm and 4dp for inch
                             var clip = (context["lengthUnits"] == "mm") ? 3 : 4;
-                            var clipFormat = clip == 1 ? "{0}{1:0.000}" : "{0}{1:0.0000}";
+                            var clipFormat = clip == 1 ? "{0}{1:0.###}" : "{0}{1:0.####}";
                             value = Math.Round(value.Value, clip);
                             tokens[ix] = String.Format(clipFormat, subToken, value);
                         }
@@ -61,77 +65,106 @@ namespace GCodeClean.Processing
                 yield return tokens;
             }
         }
-        
-        public static async IAsyncEnumerable<List<string>> Augment(this IAsyncEnumerable<List<string>> tokenizedLines) {
-            var previousXYZCoords = new List<string>() {"X0.00", "Y0.00", "Z0.00"};
-            var previousIJKCoords = new List<string>() {"I0.00", "J0.00"};
 
-            await foreach (var tokens in tokenizedLines) {
-                if (tokens.IsNotCommandOrArguments()) {
+        public static async IAsyncEnumerable<List<string>> Augment(this IAsyncEnumerable<List<string>> tokenizedLines)
+        {
+            var previousCommand = "";
+            var previousXYZCoords = new List<string>() { "X", "Y", "Z" };
+            var previousIJKCoords = new List<string>() { "I", "J" };
+
+            await foreach (var tokens in tokenizedLines)
+            {
+                if (tokens.IsNotCommandOrArguments())
+                {
                     yield return tokens;
                     continue;
                 }
 
-                var hasXY = false;
-                var hasIJ = false;
-                foreach(var token in tokens) {
-                    if (token[0] == 'X' || token[0] == 'Y') {
-                        hasXY = true;
-                        break;
-                    }
+                var hasXY = tokens.Any(t => new[]{'X', 'Y'}.Contains(t[0]));
+                var hasZ = tokens.Any(t => t[0] == 'Z');
+                var hasIJ = tokens.Any(t => new[]{'I', 'J'}.Contains(t[0]));
+                var hasK = tokens.Any(t => t[0] == 'K');
+                if (hasK) {
+                    previousIJKCoords.Add("K");
                 }
-                foreach(var token in tokens) {
-                    if (token[0] == 'I' || token[0] == 'J') {
-                        hasIJ = true;
-                        break;
+
+                if (hasXY || hasZ || hasIJ || hasK) {
+                    if (tokens.HasMovementCommand())
+                    {
+                        foreach (var token in tokens)
+                        {
+                            if (Utility.MovementCommands.Contains(token))
+                            {
+                                previousCommand = token;
+                                break;
+                            }
+                        }
                     }
-                }
-                foreach(var token in tokens) {
-                    if (token[0] == 'K') {
-                        previousIJKCoords.Add("K0.00");
-                        break;
+                    else if (!string.IsNullOrWhiteSpace(previousCommand))
+                    {
+                        tokens.Insert(0, previousCommand);
                     }
                 }
 
-                for (var ix = 0; ix < previousXYZCoords.Count; ix++) {
+                for (var ix = 0; ix < previousXYZCoords.Count; ix++)
+                {
                     var newCoord = tokens.FirstOrDefault(t => t[0] == previousXYZCoords[ix][0]);
-                    if (newCoord == null) {
+                    if (newCoord == null)
+                    {
                         newCoord = previousXYZCoords[ix];
                     }
                     previousXYZCoords[ix] = newCoord;
                 }
 
-                for (var ix = 0; ix < previousIJKCoords.Count; ix++) {
+                for (var ix = 0; ix < previousIJKCoords.Count; ix++)
+                {
                     var newCoord = tokens.FirstOrDefault(t => t[0] == previousIJKCoords[ix][0]);
-                    if (newCoord == null) {
+                    if (newCoord == null)
+                    {
                         newCoord = previousIJKCoords[ix];
                     }
                     previousIJKCoords[ix] = newCoord;
                 }
 
-                if (hasXY) {
-                    for (var ix = tokens.Count - 1; ix >= 0; ix--) {
-                        if (tokens[ix][0] == 'X' || tokens[ix][0] == 'Y' || tokens[ix][0] == 'Z') {
+                if (hasXY || hasZ)
+                {
+                    for (var ix = tokens.Count - 1; ix >= 0; ix--)
+                    {
+                        if (tokens[ix][0] == 'X' || tokens[ix][0] == 'Y' || tokens[ix][0] == 'Z')
+                        {
                             tokens.RemoveAt(ix);
                         }
                     }
-                    tokens.AddRange(previousXYZCoords);
+                    tokens.AddRange(previousXYZCoords.Where(pc => pc.Length > 1));
                 }
 
-                if (hasIJ) {
-                    for (var ix = tokens.Count - 1; ix >= 0; ix--) {
-                        if (tokens[ix][0] == 'I' || tokens[ix][0] == 'J' || tokens[ix][0] == 'K') {
+                if (hasIJ || hasK)
+                {
+                    for (var ix = tokens.Count - 1; ix >= 0; ix--)
+                    {
+                        if (tokens[ix][0] == 'I' || tokens[ix][0] == 'J' || tokens[ix][0] == 'K')
+                        {
                             tokens.RemoveAt(ix);
                         }
                     }
-                    tokens.AddRange(previousIJKCoords);
+                    tokens.AddRange(previousIJKCoords.Where(pc => pc.Length > 1));
                 }
 
-                yield return tokens;                    
+                yield return tokens;
+
+                // Keep this for later but it requires understanding plane selection
+                // if (hasZ) {
+                //     var maybeZ = tokens.First(t => t[0] == 'Z').ExtractCoord();
+                //     if (maybeZ.HasValue && maybeZ.Value > 0) {
+                //         // throw in a blank line after the Z lift
+                //         yield return new List<string>();
+                //     }
+                // }
             }
         }
 
-        public static async IAsyncEnumerable<List<string>> Annotate(this IAsyncEnumerable<List<string>> tokenizedLines) {
+        public static async IAsyncEnumerable<List<string>> Annotate(this IAsyncEnumerable<List<string>> tokenizedLines)
+        {
             JObject tokenDefinitions = JObject.Parse(File.ReadAllText("tokenDefinitions.json"));
 
             var replacements = tokenDefinitions["replacements"];
@@ -140,51 +173,67 @@ namespace GCodeClean.Processing
 
             var previousTokenCodes = new List<string>();
 
-            await foreach (var tokens in tokenizedLines) {
-                if (tokens.IsNotCommandOrArguments()) {
+            await foreach (var tokens in tokenizedLines)
+            {
+                if (tokens.IsNotCommandOrArguments())
+                {
                     yield return tokens;
                     continue;
                 }
 
                 var annotationTokens = new List<string>();
                 var tokenCodes = new List<string>();
-                foreach (var token in tokens) {
+                foreach (var token in tokens)
+                {
                     var replacement = (JObject)replacements[token];
-                    if (replacement != null) {
-                        foreach(var contextToken in replacement) {
+                    if (replacement != null)
+                    {
+                        foreach (var contextToken in replacement)
+                        {
                             context[contextToken.Key] = (string)contextToken.Value;
                         }
                     }
 
                     var annotation = (string)tokenDefs[token];
-                    if (annotation == null) {
+                    if (annotation == null)
+                    {
                         var subToken = "" + token[0];
                         tokenCodes.Add(subToken);
                         annotation = (string)tokenDefs[subToken];
                         context[token[0] + "value"] = (string)token.Substring(1);
-                    } else {
+                    }
+                    else
+                    {
                         tokenCodes.Add(token);
                     }
-                    if (annotation != null) {
-                        foreach(var contextToken in context) {
+                    if (annotation != null)
+                    {
+                        foreach (var contextToken in context)
+                        {
                             annotation = annotation.Replace("{" + contextToken.Key + "}", contextToken.Value);
                         }
                         annotationTokens.Add(annotation);
                     }
                 }
                 var isDuplicate = true;
-                if (previousTokenCodes.Count != tokenCodes.Count) {
+                if (previousTokenCodes.Count != tokenCodes.Count)
+                {
                     isDuplicate = false;
-                } else {
-                    for (var ix = 0; ix < tokenCodes.Count; ix++) {
-                        if (previousTokenCodes[ix] != tokenCodes[ix]) {
+                }
+                else
+                {
+                    for (var ix = 0; ix < tokenCodes.Count; ix++)
+                    {
+                        if (previousTokenCodes[ix] != tokenCodes[ix])
+                        {
                             isDuplicate = false;
                             break;
                         }
                     }
                 }
 
-                if (!isDuplicate && annotationTokens.Count > 0) {
+                if (!isDuplicate && annotationTokens.Count > 0)
+                {
                     tokens.Add($"({string.Join(", ", annotationTokens)})");
                     previousTokenCodes = tokenCodes;
                 }
