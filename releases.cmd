19bb906d451b30f5de47d192384ae896d50e348c
Lee Humphries <lee@md8n.com>
2020-05-23 19:42:04 +0900

    Upgraded releases command file - sorta

--- releases.cmd
+++ releases.cmd
@@ -1,4 +1,23 @@
+@ECHO OFF
 dotnet restore
-dotnet publish --configuration Release /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary --runtime linux-x64 --output bin/release/netcoreapp3.1/publish --self-contained
-dotnet publish --configuration Release /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary --runtime osx-x64 --output bin/release/netcoreapp3.1/publish --self-contained
-dotnet publish --configuration Release /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary --runtime win-x64 --output bin/release/netcoreapp3.1/publish --self-contained
+
+@IF """%~1"""=="""""" (set pver=1.0.0) ELSE (set pver=%~1)
+@ECHO version set to %pver%
+
+@FOR %%r in (linux-x64 osx-x64 win-x64) DO CALL :loopbody %%r
+@ECHO Complete
+GOTO :EOF
+
+:loopbody
+@ECHO Publishing %1
+@DEL bin\release\publish\%1\*.* /F /Q /S >NUL
+@DEL bin\release\gcodeclean-%1-standalone.zip >NUL
+
+@set outDir=bin/release/publish/%1
+@set destZip=bin/release/gcodeclean-%1-standalone.zip
+
+@dotnet publish --configuration Release /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary -p:Version=%pver% --runtime %1 --output %outDir% --self-contained
+@SET psCmdA="Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory("
+@SET psCmd=%psCmdA%'%outDir%', '%destZip%');
+powershell.exe -nologo -noprofile -command %psCmd%
+GOTO :EOF
