908e7afb61ed36c4ad488e8dfa96e25831df9565
md8n <lee@md8n.com>
2022-03-29 16:06:13 +0900

    Fix bad line endings and assumption about file terseness

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -37,116 +37,46 @@ namespace GCodeCleanCLI
                 Console.WriteLine("The path to the token definitions JSON file is missing. Proper clipping and annotating of the GCode cannot be performed.");
                 return;
             }
-            if (tokenDefsPath.ToUpperInvariant() == "TOKENDEFINITIONS.JSON")
-            {
-                var entryDir = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location)
-                               ?? Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            tokenDefsPath = Program.GetCleanTokenDefsPath(tokenDefsPath);
 
-                tokenDefsPath = $"{entryDir}{Path.DirectorySeparatorChar}tokenDefinitions.json";
-            }
-
-            JsonDocument tokenDefinitions;
-            try
+            var tokenDefinitions = Program.LoadAndVerifyTokenDefs(tokenDefsPath);
+            if (tokenDefinitions == null)
             {
-                var tokenDefsSource = File.ReadAllText(tokenDefsPath);
-                tokenDefinitions = JsonDocument.Parse(tokenDefsSource);
-            }
-            catch (FileNotFoundException fileNotFoundEx)
-            {
-                Console.WriteLine($"No token definitions file was found at {tokenDefsPath}. {fileNotFoundEx.Message}");
                 return;
             }
-            catch (JsonException jsonEx)
-            {
-                Console.WriteLine($"The supplied file {tokenDefsPath} does not appear to be valid JSON. {jsonEx.Message}");
-                return;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
 
-            var inputFile = options.filename;
-            var outputFile = inputFile;
-            var inputExtension = Path.GetExtension(inputFile);
-            if (string.IsNullOrEmpty(inputExtension))
-            {
-                outputFile += "-gcc.nc";
-            }
-            else
-            {
-                outputFile = outputFile.Replace(inputExtension, "-gcc" + inputExtension, StringComparison.InvariantCultureIgnoreCase);
-            }
+            var outputFile = Program.DetermineOutputFilename(options);
             Console.WriteLine("Outputting to:" + outputFile);
 
-            if (options.tolerance < 0.00005M)
-            {
-                options.tolerance = 0.00005M;
-            }
-            else if (options.tolerance > 0.5M)
-            {
-                options.tolerance = 0.5M;
-            }
-            Console.WriteLine("Clipping and general mathematical tolerance:" + options.tolerance);
-
-            if (options.arcTolerance < 0.00005M)
-            {
-                options.arcTolerance = 0.00005M;
-            }
-            else if (options.arcTolerance > 0.5M)
-            {
-                options.arcTolerance = 0.5M;
-            }
-            Console.WriteLine("Arc simplification tolerance:" + options.arcTolerance);
-
-            if (options.zClamp > 10.0M)
-            {
-                options.zClamp = 10.0M;
-            }
-            else if (options.zClamp > 0 && options.zClamp < 0.02M)
-            {
-                options.zClamp = 0.02M;
-            }
-            Console.WriteLine("Z-axis clamping value (max traveling height):" + options.zClamp);
-
+            var tolerance = Program.ConstrainOption(options.tolerance, 0.00005M, 0.5M, "Clipping and general mathematical tolerance:");
+            var arcTolerance = Program.ConstrainOption(options.arcTolerance, 0.00005M, 0.5M, "Arc simplification tolerance:");
+            var zClamp = Program.ConstrainOption(options.zClamp, 0.02M, 10.0M, "Z-axis clamping value (max traveling height):");
             Console.WriteLine("All tolerance and clamping values may be further adjusted to allow for inches vs. millimeters");
 
-            var linearToArcTolerance = options.tolerance * 10;
+            var linearToArcTolerance = tolerance * 10;
             var context = Default.Preamble();
 
+            var dedupSelection = new List<char> { 'F', 'Z' };
+            var minimisationStrategy = Program.GetMinimisationStrategy(options, dedupSelection);
+
+            var inputFile = options.filename;
             var inputLines = inputFile.ReadLinesAsync();
-            var outputLines = inputLines.TokeniseToLine()
-                //.EliminateLineNumbers()
+            var tokenisedLines = inputLines.TokeniseToLine();
+            var outputLines = (options.lineNumbers ? tokenisedLines.EliminateLineNumbers() : tokenisedLines)
                 .DedupRepeatedTokens()
-                .SingleCommandPerLine()
-                .InjectPreamble(context, options.zClamp)
                 .Augment()
-                .ZClamp(context, options.zClamp)
+                .SingleCommandPerLine()
+                .InjectPreamble(context, zClamp)
+                .ZClamp(context, zClamp)
                 .ConvertArcRadiusToCenter(context)
                 .DedupLine()
-                .SimplifyShortArcs(context, options.arcTolerance)
+                .SimplifyShortArcs(context, arcTolerance)
                 .DedupLinearToArc(context, linearToArcTolerance)
-                .Clip(context, options.tolerance)
+                .Clip(context, tolerance)
                 .DedupRepeatedTokens()
                 .DedupLine()
-                .DedupLinear(options.tolerance)
-                .DedupLinear(options.tolerance)
-                .DedupLinear(options.tolerance)
-                .DedupLinear(options.tolerance)
+                .DedupLinear(tolerance)
                 ;
-
-            var minimisationStrategy = string.IsNullOrWhiteSpace(options.minimise)
-                ? "SOFT"
-                : options.minimise.ToUpperInvariant();
-            var dedupSelection = new List<char> { 'F', 'Z' };
-            if (!string.IsNullOrWhiteSpace(options.minimise) && minimisationStrategy != "SOFT")
-            {
-                var hardList = new List<char> { 'A', 'B', 'C', 'D', 'F', 'G', 'H', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'X', 'Y', 'Z' };
-                dedupSelection = minimisationStrategy == "HARD" || minimisationStrategy == "MEDIUM"
-                    ? hardList
-                    : new List<char>(minimisationStrategy).Intersect(hardList).ToList();
-            }
 
             var minimisedLines = outputLines.DedupSelectTokens(dedupSelection);
 
@@ -158,6 +88,95 @@ namespace GCodeCleanCLI
             {
                 Console.WriteLine(line);
             }
+        }
+
+        private static string GetCleanTokenDefsPath(string tokenDefsPath)
+        {
+            if (tokenDefsPath.ToUpperInvariant() == "TOKENDEFINITIONS.JSON")
+            {
+                var entryDir = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location)
+                               ?? Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+                tokenDefsPath = $"{entryDir}{Path.DirectorySeparatorChar}tokenDefinitions.json";
+            }
+            return tokenDefsPath;
+        }
+
+        private static JsonDocument LoadAndVerifyTokenDefs(string tokenDefsPath)
+        {
+            JsonDocument tokenDefinitions;
+
+            try
+            {
+                var tokenDefsSource = File.ReadAllText(tokenDefsPath);
+                tokenDefinitions = JsonDocument.Parse(tokenDefsSource);
+            }
+            catch (FileNotFoundException fileNotFoundEx)
+            {
+                Console.WriteLine($"No token definitions file was found at {tokenDefsPath}. {fileNotFoundEx.Message}");
+                return null;
+            }
+            catch (JsonException jsonEx)
+            {
+                Console.WriteLine($"The supplied file {tokenDefsPath} does not appear to be valid JSON. {jsonEx.Message}");
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return tokenDefinitions;
+        }
+
+        private static string DetermineOutputFilename(Options options)
+        {
+            var inputFile = options.filename;
+            var outputFile = inputFile;
+
+            var inputExtension = Path.GetExtension(inputFile);
+            if (string.IsNullOrEmpty(inputExtension))
+            {
+                outputFile += "-gcc.nc";
+            }
+            else
+            {
+                outputFile = outputFile.Replace(inputExtension, "-gcc" + inputExtension, StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            return outputFile;
+        }
+
+        private static decimal ConstrainOption(decimal option, decimal min, decimal max, string msg)
+        {
+            if (option < min)
+            {
+                option = min;
+            }
+            else if (option > max)
+            {
+                option = max;
+            }
+            Console.WriteLine(msg + option);
+
+            return option;
+        }
+
+        private static string GetMinimisationStrategy(Options options, List<char> dedupSelection)
+        {
+            var minimisationStrategy = string.IsNullOrWhiteSpace(options.minimise)
+                ? "SOFT"
+                : options.minimise.ToUpperInvariant();
+            if (!string.IsNullOrWhiteSpace(options.minimise) && minimisationStrategy != "SOFT")
+            {
+                var hardList = new List<char> { 'A', 'B', 'C', 'D', 'F', 'G', 'H', 'L', 'M', 'N', 'P', 'R', 'S', 'T', 'X', 'Y', 'Z' };
+                dedupSelection = minimisationStrategy == "HARD" || minimisationStrategy == "MEDIUM"
+                    ? hardList
+                    : new List<char>(minimisationStrategy).Intersect(hardList).ToList();
+            }
+
+            return minimisationStrategy;
         }
     }
 }
--- GCodeClean/Processing/Linter.cs
+++ GCodeClean/Processing/Linter.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2020 - Lee HUMPHRIES (lee@md8n.com) and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE.txt file in the project root for details.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,6 +11,12 @@ namespace GCodeClean.Processing
 {
     public static class Linter
     {
+        /// <summary>
+        /// Ensures only one 'command' per line
+        /// </summary>
+        /// <param name="tokenisedLines"></param>
+        /// <remarks>This should only be run after `Augment`</remarks>
+        /// <returns></returns>
         public static async IAsyncEnumerable<Line> SingleCommandPerLine(this IAsyncEnumerable<Line> tokenisedLines)
         {
             await foreach (var line in tokenisedLines)
